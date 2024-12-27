19b8702a880b05accdcdd1b077157e76b38945ce
nassipkaliv <nassipkaliv@gmail.com>
2024-12-27 20:11:01 +0500

    update

--- js/app.js
+++ js/app.js
@@ -28,3 +28,31 @@ window.addEventListener('scroll', function () {
     navSite.classList.remove('scrolled'); 
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('#bn-about, #bn-examples, #bn-reviews, #bn-doctors, #bn-price');
+  const navLinks = document.querySelectorAll('.bn-nav-site-section');
+
+  function onScroll() {
+    let currentSection = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${currentSection}`) {
+        link.classList.add('active');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', onScroll);
+});
